9acb5051de5ac13731337bff13190af869848127
melese-2121 <meleseayen2016@gmail.com>
2024-05-28 17:36:17 +0300

    Deployment 2

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { ghPages } from "vite-plugin-gh-pages";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), ghPages()],
-  base: "/",
-  build: {
-    outDir: "dist",
-  },
+  plugins: [react()],
+  base: "/visual_story_share/",
 });
